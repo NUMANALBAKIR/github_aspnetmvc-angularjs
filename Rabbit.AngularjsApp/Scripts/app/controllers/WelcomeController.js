dcbf2c684740567e15b05573cb05a6434d1a3d55
Numan Al Bakir <36388982+NUMANALBAKIR@users.noreply.github.com>
2024-03-26 15:18:13 +0600

    ff

--- Rabbit.AngularjsApp/Scripts/app/controllers/WelcomeController.js
+++ Rabbit.AngularjsApp/Scripts/app/controllers/WelcomeController.js
@@ -4,5 +4,10 @@
     var app = angular.module('app');
     app.controller('WelcomeController', ['$scope', function ($scope) {
 
+            
+        $scope.dv = d3.version;
+
+        console.log('--- D3 version inside Welcome controller:', d3.version);
+
     }]);
 })(angular);
