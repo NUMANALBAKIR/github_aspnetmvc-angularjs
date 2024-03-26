6ac18084ab29c3fdf682e17fab1a62d6cdc44f75
Numan Al Bakir <36388982+NUMANALBAKIR@users.noreply.github.com>
2024-03-26 17:53:45 +0600

    jjj

---
+++ Rabbit.AngularjsApp/Scripts/app/controllers/_PartialController.js
@@ -0,0 +1,12 @@
+﻿'use strict';
+
+(function (angular) {
+    var app = angular.module('app');
+
+    app.controller('_PartialController', ['$scope', function ($scope) {
+
+        $scope.pn = 'data from partial view';
+
+
+    }]);
+})(angular);
