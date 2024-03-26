f5e0ae5be74d77dfefcbf899269680790e885876
Numan Al Bakir <36388982+NUMANALBAKIR@users.noreply.github.com>
2024-03-26 15:11:36 +0600

    aa

--- Rabbit.AngularjsApp/Scripts/app/controllers/TestController.js
+++ Rabbit.AngularjsApp/Scripts/app/controllers/TestController.js
@@ -23,15 +23,18 @@
             // d3v6 is now locally available
             console.log('--- D3 version inside controller:', d3v6.version);
 
-            // Example usage of d3v6 ---
+            //--- Example usage of d3v6 ---
+
+
+
 
             // set the dimensions and margins of the graph
-            const width = 450,
-                height = 450,
+            const width = 400,
+                height = 400,
                 margin = 40;
 
             // The radius of the pieplot is half the width or half the height (smallest one). I subtract a bit of margin.
-            const radius = Math.min(width, height) / 2 - margin
+            const radius = Math.min(width, height) / 2 - margin;
 
             // append the svg object to the div called 'my_dataviz'
             const svg = d3.select("#my_dataviz")
@@ -39,7 +42,7 @@
                 .attr("width", width)
                 .attr("height", height)
                 .append("g")
-                .attr("transform", `translate(${width / 2},${height / 2})`);
+                .attr("transform", `translate(${width / 2}, ${height / 2})`);
 
             // Create dummy data
             const data = { a: 9, b: 20, c: 30, d: 8, e: 12 }
@@ -50,8 +53,7 @@
 
             // Compute the position of each group on the pie:
             const pie = d3.pie()
-                .value(d => d[1])
-
+                .value(function (d) { return d[1] })
             const data_ready = pie(Object.entries(data))
 
             // Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
@@ -60,13 +62,16 @@
                 .data(data_ready)
                 .join('path')
                 .attr('d', d3.arc()
-                    .innerRadius(100)         // This is the size of the donut hole
+                    .innerRadius(0)
                     .outerRadius(radius)
                 )
-                .attr('fill', d => color(d.data[0]))
+                .attr('fill', function (d) { return (color(d.data[1])) })
                 .attr("stroke", "black")
                 .style("stroke-width", "2px")
                 .style("opacity", 0.7)
+
+
+
 
             // example usage ends
 
