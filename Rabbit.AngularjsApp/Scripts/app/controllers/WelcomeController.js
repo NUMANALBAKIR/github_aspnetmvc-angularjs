a10305d4bf14b7e3512a043abe0f279912b87b14
Numan Al Bakir <36388982+NUMANALBAKIR@users.noreply.github.com>
2024-03-30 20:41:36 +0600

    vkj

--- Rabbit.AngularjsApp/Scripts/app/controllers/WelcomeController.js
+++ Rabbit.AngularjsApp/Scripts/app/controllers/WelcomeController.js
@@ -2,12 +2,36 @@
 
 (function (angular) {
     var app = angular.module('app');
-    app.controller('WelcomeController', ['$scope', function ($scope) {
+    app.controller('WelcomeController', ['$scope', function ($scope) {        
 
-            
-        $scope.dv = d3.version;
+        $scope.form = {
+            YEAR: '',
+            MONTHFROM: '',
+            MONTHTO: '',
+            STOGRPCODE: '',
+            AREA: ''
+        };
 
-        console.log('--- D3 version inside Welcome controller:', d3.version);
+        // Initially set the YEAR value
+        // $scope.form.YEAR = '2019';
+
+        $scope.years = ["2019", "2020", "2021"];
+
+        //$scope.$watch('form.YEAR', function (newValue, oldValue) {
+
+        //    if (newValue !== oldValue) { // To avoid alerting on initialization
+        //        alert('year changed to: ' + newValue);
+        //    }
+
+        //});
+        //
+
+        $scope.changedYear = function () {
+            $scope.form.YEAR = $scope.selectedItem;
+            alert('New value selected: ' + $scope.form.selectedItem);
+        };
+
+
 
     }]);
 })(angular);
